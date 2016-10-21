5df4d9eaced4e3e385f87bd479dee32af62e1b81
Ahmad Al-Sajid <csesajid@gmail.com>
2016-10-21 23:27:37 +0600

    dataset generator

---
+++ ExtractEnglishTextOnly.py
@@ -0,0 +1,22 @@
+import pymongo
+import csv
+from time import clock
+start_time = clock()
+
+client = pymongo.MongoClient('localhost', 27017)
+db = client.tsadb
+cursor = db.TSAtweets.find(
+    {"$or":
+          [
+              {'lang': {'$regex': 'en', '$options': 'i'}},
+              {'lang': {'$regex': 'en-gb', '$options': 'i'}},
+          ]
+     }, {"text": 1, "_id": 0})
+print(cursor)
+# with open("EnglishTweet8.csv", "w", newline='', encoding='UTF-8') as f:   # for UTF-8 encoding
+with open("EnglishTweet.csv", "w", newline='', encoding='UTF-16') as f:     # for UTF-16 encoding
+    writer = csv.writer(f)
+    for t in cursor:
+        writer.writerow([t['text'],])
+
+print("total time :",round(clock()-start_time,2),"seconds")
