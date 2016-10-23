5ccbe371aa0f0e7be6af96a32e4e0a257295f92c
Ahmad Al-Sajid <csesajid@gmail.com>
2016-10-24 00:49:08 +0600

    update 7

--- GetDistinctUser.py
+++ GetDistinctUser.py
@@ -6,8 +6,11 @@ client = pymongo.MongoClient('localhost', 27017)
 db = client.tsadb
 # cursor to store distinct user id from db
 cursor = db.TSAtweets.distinct("user.id_str")
+# already got info about them
+already_exist = db.DistinctUser.distinct("user.id_str")
 # take every distinct user id from cursor and get their details from TSAtweets and store to new db DistinctUser
 for i in cursor:
-    db.DistinctUser.insert(db.TSAtweets.find({"user.id_str": i},  {"user": 1, "_id": 0}))
+    if i not  in already_exist:
+        db.DistinctUser.insert(db.TSAtweets.find({"user.id_str": i},  {"user": 1, "_id": 0}))
 
 print("total time :", round(clock()-start_time, 2), "seconds")
