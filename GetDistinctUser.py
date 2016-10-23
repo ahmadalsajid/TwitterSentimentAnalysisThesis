c9f45e74055bb10975d1e384b8b64cd349366511
Ahmad Al-Sajid <csesajid@gmail.com>
2016-10-23 11:40:09 +0600

    update 6

---
+++ GetDistinctUser.py
@@ -0,0 +1,13 @@
+import pymongo
+from time import clock
+start_time = clock()
+
+client = pymongo.MongoClient('localhost', 27017)
+db = client.tsadb
+# cursor to store distinct user id from db
+cursor = db.TSAtweets.distinct("user.id_str")
+# take every distinct user id from cursor and get their details from TSAtweets and store to new db DistinctUser
+for i in cursor:
+    db.DistinctUser.insert(db.TSAtweets.find({"user.id_str": i},  {"user": 1, "_id": 0}))
+
+print("total time :", round(clock()-start_time, 2), "seconds")
---
+++ GraphAdvertisementVsSentiment.py
@@ -0,0 +1,102 @@
+# http://stackoverflow.com/questions/24399820/expression-to-remove-url-links-from-twitter-tweet
+import pymongo
+from time import clock
+start_time = clock()
+import textblob
+import matplotlib.pyplot as plt
+from textblob.classifiers import NaiveBayesClassifier
+import re
+
+with open('training.csv', 'r') as f:
+    cl = NaiveBayesClassifier(f, format='csv')
+print("Training completed")
+client = pymongo.MongoClient('localhost', 27017)
+db = client.tsadb
+cursor = db.TSAtweets.find(
+    {"$or":
+          [
+              {'lang': {'$regex': 'en', '$options': 'i'}},
+              {'lang': {'$regex': 'en-gb', '$options': 'i'}},
+          ]
+     }, {"text": 1, "_id": 0})
+print('Data obtained from db')
+data = {
+    'advertisement': 0,
+    'sentiment': 0,
+}
+i=0
+for tweet in cursor:
+    result = re.sub(r"http\S+", "", tweet['text'])
+    res = cl.classify(result)
+    res = res.strip().lower()
+    data[res] += 1
+    i += 1
+    if i == 15000:
+        break
+
+print(data)
+
+# import csv
+# xfile = open('training.csv')
+# reader = csv.reader(xfile)
+# xdata = list(reader)
+# print(xdata)
+
+########################################################################################################################
+# code snippet 2: plot percentage of advertisement vs. sentiment
+
+df = {
+    'type': ['Sentiment', 'Advertisement'],
+    'count': [data['sentiment'], data['advertisement']],    # user count
+}
+
+print(df)
+total_user = data['sentiment']+data['advertisement']
+print("Total user:",total_user)
+# Create a figure with a single subplot
+f, ax = plt.subplots(1, figsize=(10,5))
+# Set bar width at 1
+bar_width = 1
+# positions of the left bar-boundaries
+bar_l = [i for i in range(len(df['count']))]
+# positions of the x-axis ticks (center of the bars as bar labels)
+tick_pos = [i+(bar_width/2) for i in bar_l]
+# Create the percentage of the total score the percentage value for each participant was
+percentage = [(i / total_user) * 100 for i in df['count']]
+# add percentage with names
+for i in range(len(percentage)):
+    tmp = df['type'][i]
+    tmp = tmp+'  '+"%.2f" % percentage[i]+'%'
+    df['type'][i] = tmp
+
+# Create a bar chart in position bar_1
+ax.bar(bar_l,
+       # using percentage data
+       percentage,
+       # labeled
+       label='Pre Score',
+       # with alpha
+       alpha=0.9,
+       # with color
+       color='#019600',
+       # with bar width
+       width=bar_width,
+       # with border color
+       edgecolor='white'
+       )
+# Set the ticks to be first names
+plt.xticks(tick_pos, df['type'])
+ax.set_ylabel("Percentage")
+# ax.set_xlabel("")
+plt.title('Sentiment VS. Advertisement')
+# Let the borders of the graphic
+plt.xlim([min(tick_pos)-bar_width, max(tick_pos)+bar_width])
+plt.ylim(0, 100)
+# rotate axis labels
+plt.setp(plt.gca().get_xticklabels(), rotation=45, horizontalalignment='right')
+# shot plot
+plt.show()
+
+########################################################################################################################
+
+print("total time :",round(clock()-start_time,2),"seconds")
